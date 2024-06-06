e52dc0e5effee26f90f68c778ac96d8cdcc4d957
Carlos F. Duarte <carlosf.duarte8@gmail.com>
2024-06-06 01:35:40 -0300

    wip: captura de eventos

--- android/src/main/java/com/bastengao/usbserialport/UsbSerialPortWrapper.java
+++ android/src/main/java/com/bastengao/usbserialport/UsbSerialPortWrapper.java
@@ -4,68 +4,94 @@ import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class UsbSerialPortWrapper implements SerialInputOutputManager.Listener {
-    private static final int WRITE_WAIT_MILLIS = 2000;
-    private static final int READ_WAIT_MILLIS = 2000;
-    private static final String DataReceivedEvent = "usbSerialPortDataReceived";
+  private static final int WRITE_WAIT_MILLIS = 2000;
+  private static final int READ_WAIT_MILLIS = 2000;
+  private static final String DataReceivedEvent = "usbSerialPortDataReceived";
+  private static final String ErrorEvent = "Error";
 
-    private int deviceId;
-    private UsbSerialPort port;
-    private EventSender sender;
-    private boolean closed = false;
-    private SerialInputOutputManager ioManager;
+  private int deviceId;
+  private UsbSerialPort port;
+  private EventSender sender;
+  private boolean closed = false;
+  private SerialInputOutputManager ioManager;
+  private ByteArrayOutputStream accumulatedData = new ByteArrayOutputStream();
 
-    UsbSerialPortWrapper(int deviceId, UsbSerialPort port, EventSender sender) {
-        this.deviceId = deviceId;
-        this.port = port;
-        this.sender = sender;
-        this.ioManager = new SerialInputOutputManager(port, this);
-        ioManager.start();
-    }
+  UsbSerialPortWrapper(int deviceId, UsbSerialPort port, EventSender sender) {
+    this.deviceId = deviceId;
+    this.port = port;
+    this.sender = sender;
+    this.ioManager = new SerialInputOutputManager(port, this);
+    ioManager.start();
+  }
 
-    public void send(byte[] data) throws IOException {
-        this.port.write(data, WRITE_WAIT_MILLIS);
-    }
+  public void send(byte[] data) throws IOException {
+    this.port.write(data, WRITE_WAIT_MILLIS);
+  }
 
-    public void onNewData(byte[] data) {
+  public void onNewData(byte[] data) {
+    try {
         WritableMap event = Arguments.createMap();
         String hex = UsbSerialportForAndroidModule.bytesToHex(data);
+        String value = new String(data, StandardCharsets.ISO_8859_1);
+        int[] byteArray = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            byteArray[i] = data[i] & 0xFF; // Converte byte para inteiro sem sinal
+        }
+
         event.putInt("deviceId", this.deviceId);
         event.putString("data", hex);
+        event.putString("value", value);
+        WritableArray byteArrayWritable = Arguments.createArray();
+        for (int byteValue : byteArray) {
+            byteArrayWritable.pushInt(byteValue);
+        }
+        event.putArray("rawData", byteArrayWritable);
         Log.d("usbserialport", hex);
         sender.sendEvent(DataReceivedEvent, event);
+    } catch (Exception e) {
+        // Handle the disconnection
+        WritableMap errorEvent = Arguments.createMap();
+        errorEvent.putInt("deviceId", this.deviceId);
+        errorEvent.putString("error", "USB disconnected");
+        Log.e("usbserialport", "USB disconnected", e);
+        sender.sendEvent(ErrorEvent, errorEvent);
+    }
+}
+
+  public void onRunError(Exception e) {
+    // TODO: implement
+  }
+
+  public void close() {
+    if (closed) {
+      return;
     }
 
-    public void onRunError(Exception e) {
-        // TODO: implement
+    if (ioManager != null) {
+      ioManager.setListener(null);
+      ioManager.stop();
     }
 
-    public void close() {
-        if (closed) {
-            return;
-        }
-
-        if(ioManager != null) {
-            ioManager.setListener(null);
-            ioManager.stop();
-        }
-
-        this.closed = true;
-        try {
-            port.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    this.closed = true;
+    try {
+      port.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public UsbSerialPort getPort() {
-      return port;
+  public UsbSerialPort getPort() {
+    return port;
   }
 }
--- android/src/main/java/com/bastengao/usbserialport/UsbSerialportForAndroidModule.java
+++ android/src/main/java/com/bastengao/usbserialport/UsbSerialportForAndroidModule.java
@@ -3,13 +3,12 @@ package com.bastengao.usbserialport;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.hardware.usb.UsbConstants;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbEndpoint;
-import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
-import android.util.Base64;
+import android.hardware.usb.UsbConstants;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbEndpoint;
 
 import androidx.annotation.NonNull;
 
@@ -22,22 +21,24 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
 import com.hoho.android.usbserial.driver.CdcAcmSerialDriver;
 import com.hoho.android.usbserial.driver.Ch34xSerialDriver;
 import com.hoho.android.usbserial.driver.Cp21xxSerialDriver;
 import com.hoho.android.usbserial.driver.FtdiSerialDriver;
 import com.hoho.android.usbserial.driver.ProbeTable;
-import com.hoho.android.usbserial.driver.UsbSerialDriver;
-import com.hoho.android.usbserial.driver.UsbSerialPort;
-import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import android.util.Base64;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 @ReactModule(name = UsbSerialportForAndroidModule.NAME)
-public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule {
+public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule implements EventSender {
   public static final String NAME = "UsbSerialportForAndroid";
   private static final String INTENT_ACTION_GRANT_USB = BuildConfig.LIBRARY_PACKAGE_NAME + ".GRANT_USB";
 
@@ -51,7 +52,7 @@ public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule {
   public static final String CODE_DEVICE_NOT_OPEN_OR_CLOSED = "device_not_open_or_closed";
 
   private final ReactApplicationContext reactContext;
-  private final Map<Integer, UsbSerialPortWrapper> usbSerialPorts = new HashMap<>();
+  private final Map<Integer, UsbSerialPortWrapper> usbSerialPorts = new HashMap<Integer, UsbSerialPortWrapper>();
 
   public UsbSerialportForAndroidModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -127,6 +128,7 @@ public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule {
     }
 
     promise.resolve(usbManager.hasPermission(device));
+    return;
   }
 
   @ReactMethod
@@ -212,10 +214,12 @@ public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule {
             int len = wrapper.getPort().read(buffer, 1000);
             if (len > 0) {
               String data = new String(buffer, 0, len, StandardCharsets.UTF_8);
-              sendEventToJs("onReceivedData", data);
+              WritableMap event = Arguments.createMap();
+              event.putString("data", data);
+              sendEvent("usbSerialPortDataReceived", event);
             }
           } catch (IOException e) {
-            sendEventToJs("onError", e.getMessage());
+            e.printStackTrace();
             break;
           }
         }
@@ -223,10 +227,58 @@ public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule {
     }).start();
   }
 
-  private void sendEventToJs(String eventName, String eventData) {
-    WritableMap params = Arguments.createMap();
-    params.putString("data", eventData);
-    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+  @ReactMethod
+  public void send(int deviceId, String hexStr, Promise promise) {
+    UsbSerialPortWrapper wrapper = usbSerialPorts.get(deviceId);
+    if (wrapper == null) {
+      promise.reject(CODE_DEVICE_NOT_OPEN, "device not open");
+      return;
+    }
+
+    // Enviar o comando como string
+    String command = hexStr + "\r\n"; // Adiciona uma nova linha ao comando
+    byte[] data = command.getBytes();
+
+    try {
+      // Enviar o comando
+      wrapper.send(data);
+
+      // Esperar e ler a resposta do dispositivo
+      byte[] buffer = new byte[1024]; // Tamanho do buffer de leitura
+      int numBytesRead = wrapper.getPort().read(buffer, 2000); // Timeout de 2000ms para ler a resposta
+      if (numBytesRead > 0) {
+        String response = new String(buffer, 0, numBytesRead);
+        promise.resolve(response); // Resolvendo a Promise com a resposta do dispositivo
+      } else {
+        promise.reject(CODE_SEND_FAILED, "no response from device");
+      }
+    } catch (IOException e) {
+      promise.reject(CODE_SEND_FAILED, "send failed", e);
+      return;
+    }
+  }
+
+  @ReactMethod
+  public void close(int deviceId, Promise promise) {
+    UsbSerialPortWrapper wrapper = usbSerialPorts.get(deviceId);
+    if (wrapper == null) {
+      promise.reject(CODE_DEVICE_NOT_OPEN_OR_CLOSED, "serial port not open or closed");
+      return;
+    }
+
+    wrapper.close();
+    usbSerialPorts.remove(deviceId);
+    promise.resolve(null);
+  }
+
+  public void sendEvent(final String eventName, final WritableMap event) {
+    reactContext.runOnUiQueueThread(new Runnable() {
+      @Override
+      public void run() {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, event);
+      }
+    });
   }
 
   private UsbDevice findDevice(int deviceId) {
@@ -236,61 +288,138 @@ public class UsbSerialportForAndroidModule extends ReactContextBaseJavaModule {
         return device;
       }
     }
+
     return null;
   }
 
-  @ReactMethod
-  public void send(int deviceId, String data, Promise promise) {
-    UsbSerialPortWrapper wrapper = usbSerialPorts.get(deviceId);
-    if (wrapper == null) {
-      promise.reject(CODE_DEVICE_NOT_OPEN, "device not open");
-      return;
+  public static byte[] hexStringToByteArray(String s) {
+    int len = s.length();
+    byte[] data = new byte[len / 2];
+    for (int i = 0; i < len; i += 2) {
+      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+          + Character.digit(s.charAt(i + 1), 16));
     }
+    return data;
+  }
 
-    byte[] buffer = Base64.decode(data, Base64.DEFAULT);
+  private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
-    try {
-      wrapper.getPort().write(buffer, 1000);
-      promise.resolve(null);
-    } catch (IOException e) {
-      promise.reject(CODE_SEND_FAILED, "send failed", e);
+  public static String bytesToHex(byte[] bytes) {
+    char[] hexChars = new char[bytes.length * 2];
+    for (int j = 0; j < bytes.length; j++) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+      hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
     }
+    return new String(hexChars);
   }
 
   @ReactMethod
-  public void close(int deviceId, Promise promise) {
+public void startListening(int vendorId, int productId) {
+  // Usar a libusb para detectar o dispositivo com base no Vendor ID e Product ID
+  UsbManager usbManager = (UsbManager) reactContext.getSystemService(Context.USB_SERVICE);
+  UsbDevice device = null;
+  for (UsbDevice usbDevice : usbManager.getDeviceList().values()) {
+    if (usbDevice.getVendorId() == vendorId && usbDevice.getProductId() == productId) {
+      device = usbDevice;
+      break;
+    }
+  }
+
+  if (device == null) {
+    // Dispositivo não encontrado
+    return;
+  }
+
+  // Abrir a conexão com o dispositivo
+  UsbDeviceConnection connection = usbManager.openDevice(device);
+  if (connection == null) {
+    // Não foi possível abrir a conexão com o dispositivo
+    return;
+  }
+
+  // Reivindicar a interface do dispositivo
+  UsbInterface usbInterface = device.getInterface(0); // A interface pode variar, dependendo do dispositivo
+  if (!connection.claimInterface(usbInterface, true)) {
+    // Não foi possível reivindicar a interface do dispositivo
+    connection.close();
+    return;
+  }
+
+  // Encontrar e abrir o endpoint de entrada
+  UsbEndpoint endpointIn = null;
+  for (int i = 0; i < usbInterface.getEndpointCount(); i++) {
+    UsbEndpoint endpoint = usbInterface.getEndpoint(i);
+    if (endpoint.getType() == UsbConstants.USB_ENDPOINT_XFER_BULK
+        && endpoint.getDirection() == UsbConstants.USB_DIR_IN) {
+      endpointIn = endpoint;
+      break;
+    }
+  }
+
+  if (endpointIn == null) {
+    // Endpoint de entrada não encontrado
+    connection.releaseInterface(usbInterface);
+    connection.close();
+    return;
+  }
+
+  // Iniciar uma thread para ler os dados do endpoint de entrada
+  UsbEndpoint finalEndpointIn = endpointIn;
+  new Thread(new Runnable() {
+    @Override
+    public void run() {
+      byte[] buffer = new byte[1024]; // Tamanho do buffer de leitura
+      while (true) { // Loop infinito para ouvir constantemente
+        int numBytesRead = connection.bulkTransfer(finalEndpointIn, buffer, buffer.length, 1000); // Timeout de 1000ms
+        if (numBytesRead > 0) {
+          // Se dados foram lidos, notificar a camada JavaScript com os dados recebidos
+          String data = new String(buffer, 0, numBytesRead);
+          WritableMap event = Arguments.createMap();
+          event.putString("data", data);
+          sendEvent("usbDataReceived", event);
+        }
+      }
+    }
+  }).start();
+}
+
+  @ReactMethod
+  public void writeToUsbPort(final int deviceId, final String command, final Promise promise) {
     UsbSerialPortWrapper wrapper = usbSerialPorts.get(deviceId);
-    if (wrapper == null) {
-      promise.resolve(null);
+    if (wrapper == null || wrapper.getPort() == null) {
+      promise.reject("E_PORT_NOT_OPEN", "The USB port is not open or the device is not available");
       return;
     }
 
     try {
-      wrapper.getPort().close();
-      usbSerialPorts.remove(deviceId);
-      promise.resolve(null);
+      byte[] data = command.getBytes(StandardCharsets.UTF_8);
+      wrapper.getPort().write(data, 1000); // Timeout de 1000ms
+      System.out.println("Sent command module: " + command);
+
+      // Buffer para ler a resposta
+      StringBuilder responseBuilder = new StringBuilder();
+      byte[] buffer = new byte[2024];
+      int len;
+
+      // Continue reading until no data is received for 1 second
+      long lastReadTime = System.currentTimeMillis();
+      while (System.currentTimeMillis() - lastReadTime < 1000) {
+        len = wrapper.getPort().read(buffer, 1000); // Timeout de 1000ms para cada leitura
+        if (len > 0) {
+          lastReadTime = System.currentTimeMillis(); // Reset the timer on successful read
+          responseBuilder.append(new String(buffer, 0, len, StandardCharsets.UTF_8));
+        } else {
+          // If no data is read, break the loop
+          break;
+        }
+      }
+
+      String response = responseBuilder.toString();
+      System.out.println("Received response module: " + response);
+      promise.resolve(response);
     } catch (IOException e) {
-      promise.reject(CODE_DEVICE_NOT_OPEN_OR_CLOSED, "close failed", e);
-    }
-  }
-
-  private static class UsbSerialPortWrapper {
-    private final int deviceId;
-    private final UsbSerialPort port;
-    private final UsbSerialportForAndroidModule module;
-
-    UsbSerialPortWrapper(int deviceId, UsbSerialPort port, UsbSerialportForAndroidModule module) {
-      this.deviceId = deviceId;
-      this.port = port;
-      this.module = module;
-    }
-
-    int getDeviceId() {
-      return deviceId;
-    }
-
-    UsbSerialPort getPort() {
-      return port;
+      promise.reject("E_WRITE_ERROR", "Failed to write command to USB port", e);
     }
   }
 }
